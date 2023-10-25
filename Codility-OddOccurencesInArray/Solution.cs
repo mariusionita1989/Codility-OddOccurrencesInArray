5ebb9ffe7f624797fc3b03b22640d8e87f73ba2a
Marius <marius.ionita.portfolio@outlook.com>
2023-10-25 20:58:25 +0300

    Add validation for the length of the array

--- Codility-OddOccurencesInArray/Solution.cs
+++ Codility-OddOccurencesInArray/Solution.cs
@@ -4,14 +4,21 @@ namespace Codility_OddOccurrencesInArray
 {
     public class Solution
     {
+        private const int RANGE_LOWEST_VALUE = 1;
+        private const int RANGE_HIGHEST_VALUE = 1000000;
         [MethodImpl(MethodImplOptions.AggressiveOptimization)]
         public int solution(int[] A)
         {
-            int result = 0;
-            foreach (int num in A)   // if you XOR all items from the array the paired values will be 0
-                result ^= num;       // and you will get the value that has no pair 
-                                     // if we XOR 9^9 this will be  1001^            
-            return result;           //                             1001      
-        }                            //                             ---- 
-    }                                //                             0000 = 0
-}                                    // if we XOR 0^7 the result will be 7 and that is how we can gen the value that has no pair
+            if (A.Length >= RANGE_LOWEST_VALUE && A.Length <= RANGE_HIGHEST_VALUE) 
+            {
+                int result = 0;
+                foreach (int num in A)   // if you XOR all items from the array the paired values will be 0
+                    result ^= num;       // and you will get the value that has no pair 
+                                         // if we XOR 9^9 this will be  1001^            
+                return result;           //                             1001      
+            }                            //                             ----
+            return 0;                    //                             0000 = 0
+        }                                // if we XOR 0^7 the result will be 7 and that is how we can gen the value that has no pair
+    }                                    
+}
+            
